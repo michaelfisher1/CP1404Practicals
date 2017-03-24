7d7548f0f68dae05f87f5e6416cbb401c0c3bd2b
JC225828 <michael.fisher1@my.jcu.edu.au>
2017-03-24 14:25:30 +1000

    finish list excersise

---
+++ Prac04/intermediate_excersises.py
@@ -0,0 +1,20 @@
+def main():
+    numbers = []
+    for number in range(1,6):
+        user_number = int(input("Enter number {}:".format(number)))
+        numbers.append(user_number)
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45''BaseInterpreterInterface', 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = input("Enter your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+main()
