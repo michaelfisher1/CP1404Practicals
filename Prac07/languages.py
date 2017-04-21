5e4aabceae0ade3ba508bf856135ec89b408d395
jc225828 <michael.fisher1@my.jcu.edu.au>
2017-04-21 14:18:06 +1000

    finish languages.py

---
+++ Prac07/languages.py
@@ -0,0 +1,16 @@
+from Prac07.programminglanguage import ProgrammingLanguage
+
+def main():
+    ruby = ProgrammingLanguage("Ruby","Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1995)
+    vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(ruby)
+    print(python)
+    print(vb)
+    languages = [ruby,python,vb]
+    print(["{}".format(language.name) for language in languages if language.is_dynamic()])
+    print("The dynamically typed languages are: ")
+    for language in languages:
+        if language.is_dynamic():
+            print("{}".format(language.name))
+main()
