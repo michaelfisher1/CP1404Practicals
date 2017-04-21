c0f49e623634c6138bd8d9a69ecfee9e0fb9a228
jc225828 <michael.fisher1@my.jcu.edu.au>
2017-04-21 12:07:20 +1000

    finish dynamic attempt

---
+++ Prac06/dynamic_attempt.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class DynamicWidgetsApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Bob", "Steve", "George", "Martha", "Edith", "Carrie"]
+
+    def build(self):
+        self.title = "Dynamic Names"
+        self.root = Builder.load_file("dynamic_attempt.kv")
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            temp_button = Button(text=name)
+            temp_button.bind(on_release=self.press_entry)
+            self.root.ids.nameBox.add_widget(temp_button)
+
+    def press_entry(self, intance):
+        name = instance.text
+        self.status_text = "{}".format(self.names[name])
+
+    def clear_all(self):
+        self.root.ids.nameBox.clear_widgets()
+
+
+DynamicWidgetsApp().run()
