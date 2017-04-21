71ae4388ffc5348416a082413683ba0c77ef718b
jc225828 <michael.fisher1@my.jcu.edu.au>
2017-04-21 13:15:47 +1000

    add car, add usedCars

---
+++ Prac07/usedCars.py
@@ -0,0 +1,22 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from Prac07.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    bus = Car(180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    print(bus)
+    limo = Car(100)
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
+    print(limo)
+
+
+main()
